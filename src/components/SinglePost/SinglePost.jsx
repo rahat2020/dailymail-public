2845d7574097984bb2d19f38f468da372141375e
rahat2020 <kazimdboktiar2020@gmail.com>
2023-10-28 23:02:50 +0600

    issues fixing -1

--- src/components/SinglePost/SinglePost.jsx
+++ src/components/SinglePost/SinglePost.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Card, Col, Container, Form, Image, Row, Spinner } from 'react-bootstrap';
 import './SinglePost.css';
 import SendIcon from '@mui/icons-material/Send';
@@ -19,48 +19,48 @@ import 'react-toastify/dist/ReactToastify.css';
 import { usePathname } from 'next/navigation';
 
 
-export const Data = [
-    {
-        id: 1,
-        title: 'Beauty of deep space. Billions of galaxies in',
-        category: 'Technology',
-        name: 'rahat',
-        cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-1-300x169.jpg',
-        profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
-        date: 'January 22, 2023',
-        needTime: '10 min'
-    },
-    {
-        id: 2,
-        title: 'Rocket Lab mission fails shortly after launch',
-        category: 'Science',
-        name: 'rahat',
-        cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-13-300x169.jpg',
-        profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
-        date: 'June 22, 2023',
-        needTime: '3 min'
-    },
-    {
-        id: 3,
-        title: 'The Morning After: Uber sets its sights on Postmates',
-        category: 'Technology',
-        name: 'rahat',
-        cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-14-300x169.jpg',
-        profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
-        date: 'March 10, 2023',
-        needTime: '9 min'
-    },
-    {
-        id: 4,
-        title: 'Sony’s Wf-sp800n Earbuds Are A Noise-canceling Alternative.',
-        category: 'Mobile',
-        cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-20-705x660.jpg',
-        profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
-        name: 'rahat',
-        date: 'March 10, 2023',
-        needTime: '4 min'
-    },
-]
+// export const Data = [
+//     {
+//         id: 1,
+//         title: 'Beauty of deep space. Billions of galaxies in',
+//         category: 'Technology',
+//         name: 'rahat',
+//         cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-1-300x169.jpg',
+//         profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
+//         date: 'January 22, 2023',
+//         needTime: '10 min'
+//     },
+//     {
+//         id: 2,
+//         title: 'Rocket Lab mission fails shortly after launch',
+//         category: 'Science',
+//         name: 'rahat',
+//         cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-13-300x169.jpg',
+//         profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
+//         date: 'June 22, 2023',
+//         needTime: '3 min'
+//     },
+//     {
+//         id: 3,
+//         title: 'The Morning After: Uber sets its sights on Postmates',
+//         category: 'Technology',
+//         name: 'rahat',
+//         cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-14-300x169.jpg',
+//         profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
+//         date: 'March 10, 2023',
+//         needTime: '9 min'
+//     },
+//     {
+//         id: 4,
+//         title: 'Sony’s Wf-sp800n Earbuds Are A Noise-canceling Alternative.',
+//         category: 'Mobile',
+//         cardImg: 'https://new.axilthemes.com/themes/blogar/wp-content/uploads/2021/01/demo_image-20-705x660.jpg',
+//         profle: 'https://secure.gravatar.com/avatar/1b70c830da30f39d5c6fab323017430c?s=50&d=mm&r=g',
+//         name: 'rahat',
+//         date: 'March 10, 2023',
+//         needTime: '4 min'
+//     },
+// ]
 
 const SinglePost = ({ params }) => {
 
@@ -69,7 +69,7 @@ const SinglePost = ({ params }) => {
     const { data: item } = useGetAllPostQuery(undefined)
     const filteredData = item?.filter((item) => item?.status === "approved")
     const userEmail = typeof window !== "undefined" ? window.localStorage.getItem('user') || '' : false;
-    const activeUser = typeof window !== "undefined" ? window.localStorage.getItem('Imin') || '' : false;
+    // const activeUser = typeof window !== "undefined" ? window.localStorage.getItem('Imin') || '' : false;
     const [CommetsData] = useCreateCommentsMutation()
     const { data: userData } = useUserDataByEmailQuery(userEmail)
     const [LikesData] = useCreateLikesMutation()
