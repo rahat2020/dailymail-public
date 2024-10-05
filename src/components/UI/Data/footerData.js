1ad3148756c57b262a850ea72cdb9a05f411a648
rahat2020 <bloodyhell4040@gmail.com>
2024-10-05 19:05:11 +0600

    footer social icons design updated

--- src/components/UI/Data/footerData.js
+++ src/components/UI/Data/footerData.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { FacebookIcon, InstagramIcon, LinkedinIcon, TwitterIcon } from '../customSvg';
+import { FacebookIcon, LinkedinIcon, TwitterIcon } from '../customSvg';
 
 export const companyLinks = [
     { title: 'Contact', path: '/Contact' },
@@ -16,7 +16,6 @@ export const quickLinks = [
   ];
 
 export const socialLinks = [
-  { platform: 'Instagram', url: 'https://www.instagram.com/kazirahat1020', IconComponent: InstagramIcon },
   { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/kazi-rahat2020/', IconComponent: LinkedinIcon },
   { platform: 'Facebook', url: 'https://www.facebook.com/rahatwebdev', IconComponent: FacebookIcon },
   { platform: 'Twitter', url: 'https://twitter.com/KaziRahat2020', IconComponent: TwitterIcon },
